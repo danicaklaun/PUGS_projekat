7f1d9bd0749bf716432921036278085e711e3b2c
Danica Todorovic <148051191+danicaklaun@users.noreply.github.com>
2026-07-03 16:25:19 +0200

    adding microservices

---
+++ backend/TravelPlanner/TravelPlanner.Shared/Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Remoting;
+
+using TravelPlanner.Shared.DTOs;
+
+namespace TravelPlanner.Shared.Interfaces;
+
+public interface IAuthService : IService
+{
+    Task<AuthResponseDto> RegisterAsync(RegisterRequestDto request);
+}
