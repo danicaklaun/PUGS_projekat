ea656cad6ce375e649628bbe9643f55bc56fcb76
Danica Todorovic <148051191+danicaklaun@users.noreply.github.com>
2026-07-03 16:41:43 +0200

    models update

---
+++ backend/TravelPlanner/TravelPlanner.Shared/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPlanner.Shared.Enums;
+
+namespace TravelPlanner.Shared.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public UserRole Role { get; set; } = UserRole.User;
+}
